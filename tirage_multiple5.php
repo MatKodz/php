83cba325068d6889cca8b8b821996c5c096afa8b
Mat@kodz <32668924+MatKodz@users.noreply.github.com>
2019-10-18 13:53:57 +0200

    Create tirage_multiple5.php

---
+++ tirage_multiple5.php
@@ -0,0 +1,28 @@
+<?php
+
+$tirage = array();
+$multiple5 = array();
+
+for ($i=0;$i < 10 ; $i++) {
+  $nb = rand(0,100);
+  //$tirage .= "<p>#$i : $nb </p>";
+  $tirage[] = $nb;
+  if ($nb % 5 == 0)
+  $multiple5[] = $nb;
+  //$multiple5 .= "<p> $nb </p>";
+}
+
+foreach ($tirage as $key => $value) {
+  echo "<p>#" . $key .":". $value ."</p>";
+}
+
+echo "<h2>Multiple de 5</h2>";
+
+foreach ($multiple5 as $value) {
+  echo "<p>" . $value ."</p>";
+}
+
+//echo "<div><h3>Tirage</h3>" . $tirage . "</div>";
+//echo "<div><h2>Les multiples de 5</h2>" . $multiple5 . "</div>";
+
+ ?>
