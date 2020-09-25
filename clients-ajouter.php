30f59ed94a67e7a7450287764a884def5a38baf4
Mat@kodz <32668924+MatKodz@users.noreply.github.com>
2020-09-25 16:18:32 +0200

    Update clients-ajouter.php

--- clients-ajouter.php
+++ clients-ajouter.php
@@ -1,24 +1,104 @@
+
 <?php
-print_r($_POST);
 if( isset($_POST['envoi'])) {
   $erreur = false;
   foreach ($_POST as $value) {
-    if(trim(empty($value)))
+    if (empty($value))
     $erreur = true;
-    // vérification que tous les champs de formulaire sont remplis, si ce n'est pas le cas, la variable erreur prend la valeur VRAI
   }
+  
+  // verifier si le form est complet
 
-  if($erreur == false){
-    $req_ajout = "INSERT INTO customer_list (CUS_lastname, CUS_firstname, CUS_email, CUS_password, CUS_phone, CUS_address, CUS_zipcode, CUS_town, CUS_commuting, CUS_info, CUS_register) VALUES (?,?,?,MD5(?),?,?,?,?,?,?,NOW())";
+  if( isset($_POST['nom']) && !preg_match('/^[a-zA-ZÀ-ÿ\s]{2,}$/',$_POST['nom']) )
+ {
+   $Vnom = false;
+    // vérifier si le nom est conforme (minimum 2 caractères)
+ }
+   else {
+     $Vnom = true;
+   }
 
-    require("connect.php");
-    $sth = $dbh->prepare($req_ajout);
-    $sth->execute(array($_POST['nom'],$_POST['prenom'],$_POST['monmail'],$_POST['monmotdepasse'],$_POST['telephone'],$_POST['adresse'],$_POST['codepostal'],$_POST['ville'],$_POST['locomotion'],$_POST['info']));
-    // éxecute la requête préparée avec les valeurs présentes dans le tableau array()
-    echo "Votre numéro d'enregistrement : ". $dbh->lastInsertId(); // renvoie le dernier id générée par la BDD
-  }
+   if ( isset($_POST['monmotdepasse']) && preg_match("/^(?=.*[A-Z])(?=.*\d)([0-9a-zA-Z]+)$/", $_POST['monmotdepasse'] ) ) {
+     $mdp = true;
+     // vérifier si le mot de passe est conformeMaj + min + chiffre
+   }
+   else $mdp = false;
+
+
+  if (!$erreur and ($Vnom == true) and ($mdp == true))
+
+     {
+         require "connect-bdd.php";
+    
+    // vérifier si l'adresse mail n'est pas déjà dans la BDD (le form est complet et les formats sont respectés
+
+           $requete_v = "SELECT CUS_id FROM customer_list WHERE CUS_email = ? ";
+
+           $sth = $conn->prepare($requete_v);
+
+           $sth->execute([trim($_POST['monmail'])]);
+
+           $reponse = $sth->fetch();
+
+           if($reponse)
+           {
+            $msg = 'L\'email renseigné est deja utilisé par un autre utilisateur';
+            $mail_class = "bg bg-danger text-white";
+            // si la requête renvoie un résultat, cela veut dire que le mail est déjà utilisé
+           }
+
+            else {
+      
+            // sinon - adresse mail n'est pas dans la BDD on fait l'insertion
+
+           echo '<div class="container"><div class="row"><div class="col-sm"><p class="alert alert-success">Nous avons bien pris en compte vos coordonnées</p></div>';
+
+           foreach ($_POST as $key => $value) {
+
+             $coordonnees[$key]=$value;
+             // on stocke les valeurs dans un nouveau tableau coordonnees
+
+           }
+
+
+          // paramètre de connexion à la base
+          require "connect-bdd.php";
+
+         $requete_enregistrement = "INSERT INTO customer_list (CUS_lastname, CUS_firstname, CUS_email, CUS_password, CUS_phone, CUS_address, CUS_zipcode, CUS_town, CUS_commuting, CUS_info, CUS_register)
+         VALUES (:nom, :prenom, :courriel, MD5(:mdp), :telephone, :adresse, :codepostal, :ville, :deplacement, :information, NOW())";
+
+         $sth = $conn->prepare($requete_enregistrement, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+         $sth->execute(array(':nom' => $coordonnees['nom'],
+                              ':prenom' => $coordonnees['prenom'],
+                              ':courriel' => $coordonnees['monmail'],
+                              ':mdp' => $coordonnees['monmotdepasse'],
+                              ':telephone' => $coordonnees['telephone'],
+                              ':adresse' => $coordonnees['adresse'],
+                              ':codepostal' => $coordonnees['codepostal'],
+                              ':ville' => $coordonnees['ville'],
+                              ':deplacement' => $coordonnees['locomotion'],
+                              ':information' => $coordonnees['info'],
+                            ));
+
+          echo '<div class="col-sm-12"><h2>Merci</h2>Les coordonnées ont été intégrées à nos fichiers client. Merci de votre confiance et à bientôt.</div><div></div>';
+
+          $form_fadeOut =  "<script> form_hidden(); </script>";
+
+          $conn = NULL;
+
+         }
+
+   }
 }
+
+
+
+
+
 ?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -36,6 +116,7 @@ if( isset($_POST['envoi'])) {
 
   function form_hidden() {
     document.getElementById('form').style.display = "none";
+    document.querySelector(".jumbotron").style.display = "none";
   };
 
   </script>
@@ -65,9 +146,7 @@ if( isset($_POST['envoi'])) {
   input:required { border-left: 2px solid orange;}
 
   .does-exist {
-    position: fixed;
-    top: 7%;
-    left: 40%;
+
   }
 
 
@@ -97,68 +176,103 @@ if( isset($_POST['envoi'])) {
 
   <div class="row">
 
+    <?php if (isset($msg))
+    echo '<p class="alert alert-danger does-exist">'.  $msg . '</p>';
+    ?>
+
     <form name="formulaire" id="form" method="post" action="">
     <h3>S'inscrire comme client </h3>
     <p> Tous les champs sont obligatoires</p>
 
   <div class="col-">
     <label id="lanom" for="Votre nom">Votre nom</label>
-    <input type="text" size="25" name="nom" id="lanom" placeholder="Votre nom ici"   value="">
-          </div>
+    <input type="text" size="25" name="nom" id="lanom" placeholder="Votre nom ici" <? /*pattern="[a-zA-Z]{2,20}"*/?>  value="<?php if((isset($_POST['nom']))) { echo $_POST['nom'];}?>">
+        <?php
+        if( isset($_POST['nom']) && empty($_POST['nom']) )
+        echo "<span>Le nom est manquant</span>";
+         else if( isset($_POST['nom']) && !preg_match('/^[a-zA-ZÀ-ÿ\s]{2,}$/',$_POST['nom']) )
+        {
+          echo "<span class=\"alert alert-warning\">Minimum 2 lettres. Les caractères spéciaux ne sont pas autorisés</span>";
+        }
+        ?>
+  </div>
 
   <div class="col-">
     <label id="laprenom" for="Votre prénom">Votre prénom</label>
-    <input type="text" size="25" name="prenom" id="laprenom" placeholder="Votre prénom ici"  value="">
-          </div>
+    <input type="text" size="25" name="prenom" id="laprenom" placeholder="Votre prénom ici" <? /*pattern="[a-zA-Z]{2,20}" */?> value="<?php if((isset($_POST['prenom']))) { echo $_POST['prenom'];}?>">
+        <?php if((isset($_POST['prenom'])) && empty(trim($_POST['prenom']) ) ) { echo "<span>Le prénom est manquant</span>";} ?>
+  </div>
 
-  <div class="col-">
+  <div class="col-  <?php if(isset($mail_class)) echo $mail_class; ?>">
   <label id="lamail" for="Votre email">Votre email</label>
-  <input type="email" id="lamail" size="30" name="monmail" placeholder="Votre mail ici" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" value="">
+  <input type="email" id="lamail" size="30" name="monmail" placeholder="Votre mail ici" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" value="<?php if((isset($_POST['monmail']))) { echo $_POST['monmail'];}?>">
+      <?php
+      if( isset($_POST['monmail']) && empty($_POST['monmail']) ) {
+        echo "<span>Le mail est manquant</span>";
+      }
+      else if( isset($_POST['monmotdepasse']) && !filter_var($_POST['monmail'], FILTER_VALIDATE_EMAIL) )
+      { echo "<span>Email invalide</span>";}
+      ?>
 
   </div>
 
   <div class="col-">
   <label id="pwd" for="Votre mot de passe">Votre mot de passe</label>
-  <input type="password" id="pwd" size="20" name="monmotdepasse" value="">
-        </div>
+  <input type="password" id="pwd" size="20" name="monmotdepasse" value="<?php if((isset($_POST['monmotdepasse']))) { echo $_POST['monmotdepasse'];}?>">
+      <?php if( isset($_POST['monmotdepasse']) && empty($_POST['monmotdepasse']) )
+        echo "<span>Le mot de passe est requis</span>";
+        else if ( isset($_POST['monmotdepasse']) && !preg_match("/^(?=.*[A-Z])(?=.*\d)([0-9a-zA-Z]+)$/", $_POST['monmotdepasse'] ) )
+        {
+          echo '<p class="alert alert-warning">Mot de passe non conforme (une Maj, une Min, un chiffre minimum)</p>';
+        }
+
+?>
+  </div>
 
 
   <div class="col-">
     <label id="lanum" for="Votre numéro de téléphone">Votre numéro de téléphone</label>
-    <input type="text" size="25" name="telephone" id="lanum" placeholder="Votre numéro de téléphone ici" pattern="[0-9]{10}" value="">
-          </div>
+    <input type="text" size="25" name="telephone" id="lanum" placeholder="Votre numéro de téléphone ici" pattern="[0-9]{10}" value="<?php if((isset($_POST['telephone']))) { echo $_POST['telephone'];}?>">
+        <?php if((isset($_POST['telephone'])) && empty($_POST['telephone']) ) { echo "<span>Le numéro de téléphone est manquant</span>";} ?>
+  </div>
 
 
   <div class="col-">
     <label id="laadresse" for="Votre adresse">Votre adresse</label>
-    <input type="text" size="25" name="adresse" id="lanum" placeholder="Votre adresse" value="">
-          </div>
+    <input type="text" size="25" name="adresse" id="lanum" placeholder="Votre adresse" value="<?php if((isset($_POST['adresse']))) { echo $_POST['adresse'];}?>">
+        <?php if((isset($_POST['adresse'])) && empty($_POST['adresse']) ) { echo "<span>L'adresse est manquante</span>";} ?>
+  </div>
 
 
   <div class="col-">
     <label id="lacp" for="Votre CP">Votre code postal</label>
-    <input type="text" size="5" name="codepostal" id="lacp" placeholder="Votre CP" value="">
-          </div>
+    <input type="text" size="5" name="codepostal" id="lacp" placeholder="Votre CP" value="<?php if((isset($_POST['codepostal']))) { echo $_POST['codepostal'];}?>">
+        <?php if((isset($_POST['codepostal'])) && empty($_POST['codepostal']) ) { echo "<span>Le CP est manquant</span>";} ?>
+  </div>
 
 
 
   <div class="col-">
   <label id="laville" for="Votre ville">Votre ville de résidence</label>
-  <input type="text" size="25" name="ville" id="laville" placeholder="Votre ville ici" value="">
-        </div>
+  <input type="text" size="25" name="ville" id="laville" placeholder="Votre ville ici" value="<?php if((isset($_POST['ville']))) { echo $_POST['ville'];}?>">
+      <?php if((isset($_POST['ville'])) && $_POST['ville'] == "") { echo "<span>La ville est manquante</span>";} ?>
+  </div>
 
   <div class="col-">
   <label>Votre mode de déplacement</label>
-  <input type="radio" name="locomotion" value="car" id="voit" ><label for="voit">Voiture</label>
-  <input type="radio" name="locomotion" value="bike" id="vel" > <label for="vel">Vélo</label>
-  <input type="radio" name="locomotion" value="taxi" id="tax" > <label for="tax">Taxi</label>
-  <input type="radio" name="locomotion" value="plane" id="avi" > <label for="avi">Avion</label>
-        </div>
+  <input type="radio" name="locomotion" value="car" id="voit" <? if(isset($_POST['locomotion']) && $_POST['locomotion'] == 'car') echo "checked" ?>><label for="voit">Voiture</label>
+  <input type="radio" name="locomotion" value="bike" id="vel" <? if(isset($_POST['locomotion']) && $_POST['locomotion'] == 'bike') echo "checked" ?>> <label for="vel">Vélo</label>
+  <input type="radio" name="locomotion" value="taxi" id="tax" <? if(isset($_POST['locomotion']) && $_POST['locomotion'] == 'taxi') echo "checked" ?>> <label for="tax">Taxi</label>
+  <input type="radio" name="locomotion" value="plane" id="avi" <? if(isset($_POST['locomotion']) && $_POST['locomotion'] == 'plane') echo "checked" ?>> <label for="avi">Avion</label>
+      <?php
+      if( isset($_POST['envoi']) && !isset($_POST['locomotion']) ) { echo "<span>Le moyen de transport est manquant</span>";} ?>
+  </div>
 
   <div class="col-">
     <label id="info" for="Vos informations">Informations complémentaires</label>
-    <textarea name="info" maxlength="500"></textarea>
-          </div>
+    <textarea name="info" maxlength="500"><?php if(isset($_POST['info'])) echo trim($_POST['info']); ?></textarea>
+        <?php if((isset($_POST['info']))&&$_POST['info']=="") { echo "<span><br>Le message est vide</span>";} ?>
+  </div>
 
   <div class="col-12 text-center">
     <input type="submit" name="envoi" value="Envoyer les informations" class="btn btn-success">
@@ -168,7 +282,9 @@ if( isset($_POST['envoi'])) {
   </form>
 </div>
 </div>
-
+<?php
+if (isset($form_fadeOut)) echo $form_fadeOut;
+?>
 
 
 </body>
